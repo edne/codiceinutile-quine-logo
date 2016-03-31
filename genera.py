ea2d84f27a6dd3f24fe911c88599234f7619aaa1
Edoardo Negri <edne@gmx.com>
2016-03-31 07:29:41 +0200

    Avoid automatic space printing

--- genera.py
+++ genera.py
@@ -9,17 +9,16 @@ def genera():
     "Generate the quine code"
 
     var_name = "_"
-    separator_1 = ";"
-    separator_2 = " ; "
+    separator = ";"
     left_side = join(var_name, " = ")
-    formatting = join(r"r\"\"\"{0}\"\"\"'.format(", var_name, "), '")
+    formatting = join(r"r\"\"\"{0}\"\"\"'.format(", var_name, ") + '")
     printing = join("print('",
-                    left_side, formatting, separator_1, "', ", var_name,
+                    left_side, formatting, separator, "' + ", var_name,
                     ")")
 
     code = join(left_side,
                 "r\"\"\"", printing, "\"\"\"",
-                separator_2,
+                separator,
                 printing, "\n")
     return code
 
