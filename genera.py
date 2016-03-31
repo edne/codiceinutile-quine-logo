c734a11c939b3e964123dd358618be63cff36661
Edoardo Negri <edne@gmx.com>
2016-03-31 19:52:27 +0200

    Move functions

--- genera.py
+++ genera.py
@@ -5,29 +5,32 @@ def join(*args):
     return "".join(args)
 
 
+def escape(*args):
+    return join(*args).replace("\n", r"\n").replace("\"", r"\"")
+
+
+def quote(*args):
+    return join("'", escape(*args), "'")
+
+
+def triple_quote(*args):
+    return join("r\"\"\"", join(*args), "\"\"\"")
+
+
+def expand(n, s):
+    rows = s.split("\n")
+    for i, r in enumerate(rows):
+        space_i = r.find(" ")
+        if space_i != -1:
+            while len(r) < n:
+                r = r[:space_i] + " " + r[space_i:]
+            rows[i] = r
+
+    return "\n".join(rows)
+
+
 def genera(width=1):
     "Generate the quine code"
-
-    def escape(*args):
-        return join(*args).replace("\n", r"\n").replace("\"", r"\"")
-
-    def quote(*args):
-        return join("'", escape(*args), "'")
-
-    def triple_quote(*args):
-        return join("r\"\"\"", join(*args), "\"\"\"")
-
-    def expand(n, s):
-        rows = s.split("\n")
-        for i, r in enumerate(rows):
-            space_i = r.find(" ")
-            if space_i != -1:
-                while len(r) < n:
-                    r = r[:space_i] + " " + r[space_i:]
-                rows[i] = r
-
-        return "\n".join(rows)
-
     var_name = "_"
 
     left_side = join(var_name, "=")
@@ -52,6 +55,9 @@ def test(code, file_name):
     # can it be done with an eval?
     from subprocess import check_output
 
+    with open(file_name, "w") as f:
+        f.write(code)
+
     output = check_output(["python3", file_name]).decode()
     if code != output:
         print(output)
@@ -60,15 +66,11 @@ def test(code, file_name):
 
 def main():
     "Main function"
-    file_name = "quine.py"
     code = genera(width=2)
 
     print(code)
 
-    with open(file_name, "w") as f:
-        f.write(code)
-
-    test(code, file_name)
+    test(code, "quine.py")
 
 if __name__ == "__main__":
     main()
