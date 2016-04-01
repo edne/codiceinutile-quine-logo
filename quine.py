f4d939c7790616b4642be4fcdc86f0448b677d6b
Edoardo Negri <edne@gmx.com>
2016-04-01 11:55:08 +0200

    Restore output file

---
+++ quine.py
@@ -0,0 +1,11 @@
+_=r"""
+gro.elituniecidoc #         
+)        ]1-::[_+_         %
+         '"\"\"\s%"\"\"\r=_'
+         (             tnirp
+"""
+print             (         
+'_=r\"\"\"%s\"\"\"'         
+%         _+_[::-1]        )
+         # codiceinutile.org
+
